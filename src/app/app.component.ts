4f42051dc19c2b7b35659e0ee0ffc89f019f4323
mathebulaF95 <mathebulaF95@gmail.com>
2018-10-23 12:33:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { SettingsProvider } from './../providers/settings/settings';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  selectedTheme: String;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private settings: SettingsProvider ) {
+      this.settings.getActiveTheme().subscribe(val => this.selectedTheme = val);
+
+      platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
     });
